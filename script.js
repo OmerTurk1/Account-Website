16a0ee620d1b2d0d903b9de49ff9a51e9adaf88c
OmerTurk1 <oomertuurk@gmail.com>
2025-06-27 14:45:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ form.addEventListener('submit', async (e) => {
   try {
     const res = await fetch(scriptURL, {
       method: 'POST',
-      mode: 'no-cors',
       headers: {
         'Content-Type': 'text/plain;charset=utf-8'
       },
